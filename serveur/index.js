29378d3658e6f7800011045d7768ee99cb9796d5
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-07-25 03:32:22 +0200

    routeur avec paramettres

--- serveur/index.js
+++ serveur/index.js
@@ -2,9 +2,21 @@ const express = require('express'); // impoter le module express
 const app = express(); // creer une instance d'express js
 
 const router = express.Router();
-router.get('/9ahwa', (req, res) => {
-	res.send('9ahwa');
+
+router.get('/Description', (req, res) => {
+	res.send('this page is a test for serveur express');
 });
+
+router.get('/Home', (req, res) => {
+	res.send('welcome to the Home page ');
+});
+
+router.get('/:nom',(req,res)=>{
+
+    res.send(`${req.params.nom}`);
+})
+
+
 app.use('/', router);
 
 app.listen(3000, () => console.log('Serveur en marche'));
